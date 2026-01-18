f0ecae8d6699db0f3161daae0f9ae784ae66573e
Irina <irinarakoto2005@gmail.com>
2026-01-18 13:51:15 +0300

    fix: drop column id_dish and required_quantity

--- src/main/resources/ new_schema.sql
+++ src/main/resources/ new_schema.sql
@@ -8,6 +8,9 @@ create table dish_ingredient
     quantity_required numeric(10, 2),
     unit              unit_type
 );
+ALTER TABLE ingredient
+DROP COLUMN IF EXISTS id_dish,
+DROP COLUMN IF EXISTS required_quantity;
 
 insert into dish_ingredient (id, id_dish, id_ingredient, quantity_required, unit)
 values (1, 1, 1, 0.20, 'KG'),
@@ -15,6 +18,8 @@ values (1, 1, 1, 0.20, 'KG'),
        (3, 2, 3, 1.00, 'KG'),
        (4, 4, 4, 0.30, 'KG'),
        (5, 4, 5, 0.20, 'KG');
+
+SELECT setval('dish_ingredient_id_seq', (SELECT MAX(id) FROM dish_ingredient));
 
 UPDATE dish SET price = 3500.00 WHERE id = 1;
 UPDATE dish SET price = 12000.00 WHERE id = 2;
